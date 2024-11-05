3ace55bd2ece404a30304cc18e25e1438a1dea67
Nikhiliitg <d.nikhil@op.iitg.ac.in>
2024-11-05 11:38:22 +0530

    Done

---
+++ tools.py
@@ -0,0 +1,4 @@
+from crewai_tools import YoutubeChannelSearchTool
+
+# Initialize the tool with a working YouTube channel handle
+yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@MrBeast')
